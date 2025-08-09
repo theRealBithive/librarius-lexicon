2bdb439e26ccf9266954cd5c2ea5c1726964d3d1
Bithive <bithive@bithive.space>
2025-08-09 08:30:42 +0000

    Add loguru for logging, implement audiobook detection logic, and enhance import functionality in the Importer app.

--- src/apps/importer/main.py
+++ src/apps/importer/main.py
@@ -1,11 +1,28 @@
-def import_audiobooks(input_path: str):
+import os
+from loguru import logger
+from src.apps.core.models import Audiobook
 
-    pass
+logger.add("logs/importer.log", rotation="100 MB", retention="10 days", enqueue=True)
 
+def import_audiobooks():
+    """
+    This function is called by the Django Q scheduler.
+    It will import all audiobooks from the input path, which is always /app/input. To Change the path, change the docker mount point in docker-compose.yml.
+    """
+    logger.info("Importing audiobooks")
+    importer = Importer()
+    importer.run()
 
 class Importer:
     def __init__(self):
-        pass
+        self.input_path = "/app/input"
 
     def run(self):
-        pass
+        logger.info("Running Importer")
+        logger.info("Checking for new audiobooks in /app/input")
+        candidates = find_audiobooks(self.input_path)
+        logger.info(f"Found {len(candidates)} candidates")
+        for candidate in candidates:
+            logger.info(f"Processing {candidate.path}")
+
+
